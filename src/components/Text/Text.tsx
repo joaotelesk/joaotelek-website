b4870c8633d8d2651414c1ae6b3a17d3fbf5a71d
Joao Teles <joaojoseteles@gmail.com>
2023-02-09 22:27:50 -0300

    :sparkles: feat: components Hading e Text

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,35 @@
+// Components
+import { Slot } from "@radix-ui/react-slot";
+
+// Utilities
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+// Interfaces
+export interface TextProps {
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
+}
+
+export function Text({ size = "md", children, asChild, className }: TextProps) {
+  const Comp = asChild ? Slot : "span";
+  return (
+    <>
+      <Comp
+        className={clsx(
+          "text-white-500 font-normal",
+          {
+            "text-xs": size === "sm",
+            "text-sm": size === "md",
+            "text-md": size === "lg",
+          },
+          className
+        )}
+      >
+        {children}
+      </Comp>
+    </>
+  );
+}
