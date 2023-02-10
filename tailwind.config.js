2468addca09adc1080c2c208492a997beabc8cc9
Joao Teles <joaojoseteles@gmail.com>
2023-02-09 22:21:15 -0300

    :recycle: refactor: inclusao de tokens: colors, fontSize e modificaçao da fonteFamly do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,35 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 30,
+      "2xl": 36,
+    },
+    colors: {
+      black: {
+        500: "#000000",
+        400: "#111111",
+        300: "#121214",
+        200: "#1E1E1E",
+      },
+      white: {
+        500: "#FFFFFF",
+        400: "#FCFBFF",
+        300: "#D9D9D9",
+        200: "#FFFFFF99",
+      },
+      purple: {
+        500: "#8726F8",
+      },
+    },
+
     extend: {
       fontFamily: {
-        sans: "Inter, sans-serif",
+        sans: "DM Sans, sans-serif",
       },
     },
   },
