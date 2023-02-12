d8b8401829e56fcd2f41350daca19edab72ea655
Joao Teles <joaojoseteles@gmail.com>
2023-02-12 18:19:32 -0300

    :sparkles: feat: stories do component Footer

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,13 @@
+// Components
+import { Footer, FooterProps } from "./Footer";
+
+// Utilities
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Compnents/Footer",
+  component: Footer,
+  args: {},
+} as Meta<FooterProps>;
+
+export const Default: StoryObj<FooterProps> = {};
