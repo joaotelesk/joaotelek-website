972bead6c6089b492a57cf4476243285aaa54b1f
Joao Teles <joaojoseteles@gmail.com>
2023-02-13 14:09:46 -0300

    :recycle: refactor: arquivos de configuraçao do storybook com addon e no arquivo de styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,26 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: [
+    "../public",
+    "../static",
+    { from: "../foo/assets", to: "/assets" },
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
 
   framework: "@storybook/react",
