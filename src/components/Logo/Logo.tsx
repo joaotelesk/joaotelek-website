5afde3c6f494991a4effc4f44424fa16eb8a8fab
Joao Teles <joaojoseteles@gmail.com>
2023-02-13 12:09:03 -0300

    :sparkles: feat: Component Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+// Components
+import Image from "next/image";
+
+// Interfaces
+export interface LogoProps {
+  size: number;
+  className?: string;
+}
+
+export function Logo({ size, className }: LogoProps) {
+  return (
+    <>
+      <div className="flex mb-5 items-center gap-2">
+        <Image alt="Logo do site" src="/logo.svg" height={size} width={size} />
+        <span className={className}>João Teles</span>
+      </div>
+    </>
+  );
+}
