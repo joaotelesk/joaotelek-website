f12e1f7b3ad7b31c3569d7b7c9bb5bdb866399aa
Joao Teles <joaojoseteles@gmail.com>
2023-02-13 22:47:59 -0300

    :zap: perf: pagina de teste de componentes

---
+++ pages/paginaTeste.tsx
@@ -0,0 +1,23 @@
+import { CardItems } from "../src/components/CardItems/CardItems";
+import { Footer } from "../src/components/Footer/Footer";
+import { Form } from "../src/components/Form/Form";
+
+export default function Teste() {
+  return (
+    <>
+      <div className="flex flex-col  items-center justify-between h-screen w-full">
+        <div className="flex flex-wrap gap-8 text-center mt-10 justify-center items-center mb-40 px-3">
+          <CardItems />
+          <CardItems />
+          <CardItems />
+          <CardItems />
+        </div>
+
+        <div className="mx-4">
+          <Form onSubmit={() => {}} />
+        </div>
+        <Footer className="mt-10" />
+      </div>
+    </>
+  );
+}
