45f286454f3596bf5cc844dccbe10f7d04fb2ba6
Joao Teles <joaojoseteles@gmail.com>
2023-02-13 21:20:41 -0300

    :test_tube: test: unitario do component CardItems

---
+++ src/components/CardItems/CardItems.test.tsx
@@ -0,0 +1,25 @@
+// Components
+import { CardItems } from "./CardItems";
+
+// Utilities
+import { render, screen } from "@testing-library/react";
+
+describe("<CardItems/>", () => {
+  it("renders the form correctly", () => {
+    const { container } = render(<CardItems />);
+
+    // Verifica se a div de CardItems foi renderizada
+    const divContainer = container.querySelector("div");
+    expect(divContainer).toBeInTheDocument();
+
+    // Verifica se os Elemento de CardItems foram rederizados
+    const title = screen.getByText("Titulo");
+    expect(title).toBeInTheDocument();
+    const linkGitGub = screen.getByAltText("logo github");
+    expect(linkGitGub).toBeInTheDocument();
+    const linkLogoFigma = screen.getByAltText("logo figma");
+    expect(linkLogoFigma).toBeInTheDocument();
+    const linkLogoGoogle = screen.getByAltText("logo google");
+    expect(linkLogoGoogle).toBeInTheDocument();
+  });
+});
