d912dff2bb2a30608bd1a995edb03386b2d02c36
Joao Teles <joaojoseteles@gmail.com>
2023-02-14 09:54:33 -0300

    :mag: ajustando seo da paginaTeste

--- pages/paginaTeste.tsx
+++ pages/paginaTeste.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { CardItems } from "../src/components/CardItems/CardItems";
 import { Footer } from "../src/components/Footer/Footer";
 import { Form } from "../src/components/Form/Form";
@@ -5,6 +6,13 @@ import { Form } from "../src/components/Form/Form";
 export default function Teste() {
   return (
     <>
+      <Head>
+        <title>Pagina Teste</title>
+        <meta
+          name="description"
+          content="Pagina de teste de responsividade dos componentes"
+        />
+      </Head>
       <div className="flex flex-col  items-center justify-between h-screen w-full">
         <div className="flex flex-wrap gap-8 text-center mt-10 justify-center items-center mb-40 px-3">
           <CardItems />
