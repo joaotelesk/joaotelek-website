520116af66815a947259f45e32fd641f46c6c6ad
Joao Teles <joaojoseteles@gmail.com>
2023-02-13 21:22:54 -0300

    :sparkles: feat: Stories CardItems

---
+++ src/components/CardItems/CardItems.stories.tsx
@@ -0,0 +1,13 @@
+// Components
+import { CardItems, CarItemsPros } from "./CardItems";
+
+// Utilities
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/CardItems",
+  component: CardItems,
+  args: {},
+} as Meta<CarItemsPros>;
+
+export const Default: StoryObj<CarItemsPros> = {};
