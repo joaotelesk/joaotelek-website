5e42a76eae012bb3c07b2cbf9eeb11ad0a27de7f
Joao Teles <joaojoseteles@gmail.com>
2023-02-16 18:36:35 -0300

    :books: docs: Atualizaçao do next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["drive.google.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
