448c857b1435d59fa5a920fce0708c1ea5c24572
Joao Teles <joaojoseteles@gmail.com>
2023-02-16 18:38:34 -0300

    :recycle: refactor: utilizando os dados da api para renderizar cardItems

--- pages/paginaTeste.tsx
+++ pages/paginaTeste.tsx
@@ -4,8 +4,49 @@ import { CardItems } from "../src/components/CardItems/CardItems";
 import { Footer } from "../src/components/Footer/Footer";
 import { Form } from "../src/components/Form/Form";
 import { Header } from "../src/components/Header/Header";
+import { useGetProjectsQuery } from "../src/graphql/generated";
+import Skeleton from "@mui/material/Skeleton";
 
 export default function Teste() {
+  const { data, loading, error } = useGetProjectsQuery();
+  if (loading) {
+    // substituindo o conteúdo com o Skeleton enquanto o conteúdo é carregado
+    return (
+      <>
+        <Head>
+          <title>Pagina Teste</title>
+          <meta
+            name="description"
+            content="Pagina de teste de responsividade dos componentes"
+          />
+        </Head>
+        <div className="flex flex-col items-center justify-between h-screen w-full">
+          <Header />
+          <div className="flex flex-wrap gap-8 text-center justify-center items-center mb-40 px-3">
+            {[...Array(4)].map(
+              (
+                _,
+                index // renderizando Skeletons
+              ) => (
+                <Skeleton
+                  key={index}
+                  variant="rectangular"
+                  width={280}
+                  height={355}
+                />
+              )
+            )}
+          </div>
+
+          <div className="mx-4">
+            <Form />
+          </div>
+
+          <Footer className="mt-10" />
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <Head>
@@ -18,10 +59,17 @@ export default function Teste() {
       <div className="flex flex-col items-center justify-between h-screen w-full">
         <Header />
         <div className="flex flex-wrap gap-8 text-center justify-center items-center mb-40 px-3">
-          <CardItems />
-          <CardItems />
-          <CardItems />
-          <CardItems />
+          {data?.projects?.map((project) => (
+            <CardItems
+              key={project.id}
+              title={project.title}
+              urlFigma={project.urlFigma}
+              urlImg={`https://drive.google.com/uc?id=${project.urlImg}`}
+              urlGithub={project.urlGithub}
+              urlSite={project.urlSite}
+              slug={project.slug}
+            />
+          ))}
         </div>
 
         <div className="mx-4">
