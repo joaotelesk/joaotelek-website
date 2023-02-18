473bc649989f89bcd7e0a7a6a023c987ac812f6c
Joao Teles <joaojoseteles@gmail.com>
2023-02-18 18:59:03 -0300

    :recycle: refactor: meu com rota de portfolio

--- src/components/Header/MenuMob/MenuMob.tsx
+++ src/components/Header/MenuMob/MenuMob.tsx
@@ -1,12 +1,14 @@
 // Components
 import Link from "next/link";
-import { TelegramLogo } from "phosphor-react";
 import { Button } from "../../Button/Button";
-import { CardImg } from "../../CardImg/CardImg";
 import { Heading } from "../../Heading/Heading";
 import { Navbar } from "../NavBar/NavBar";
 
 // Utilities
+import { GrLinkedinOption } from "react-icons/gr";
+import { AiFillGithub } from "react-icons/ai";
+import { FaWhatsapp, FaDiscord, FaTelegramPlane } from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
 
 // Hooks
 import { useRouter } from "next/router";
@@ -32,36 +34,77 @@ export function MenuMob({ close }: MenuMobProps) {
             <Button
               role="button"
               aria-label="button de fechar o menu"
-              className="font-bold text-lg bg-black-300 border-none rounded-2xl text-purple-500 bg-tran"
+              className=" bg-black-300 border-none rounded-2xl  bg-tran"
               onClick={handleClick}
-            >
-              X
-            </Button>
+              icon={MdOutlineClose}
+              size={26}
+              colorNormal="text-purple-500"
+              colorHover="text-purple-500"
+            />
           </div>
           <Navbar className="flex flex-col w-full gap-10" />
         </div>
         <div className=" w-full  flex flex-col  mt-20 gap-4 items-center">
           <Button
             role="button"
+            icon={FaTelegramPlane}
             aria-label="button de entrar na pagina de contato"
-            className="w-[215px]"
-            onClick={() => router.push("")}
+            className="btn-primary"
+            onClick={() => router.push("/contato")}
           >
             <p className="text-lg text-bold ">Contato</p>
-            <TelegramLogo size={24} />
           </Button>
           <div className="flex gap-2">
-            <Link href="https://www.linkedin.com/in/joaotelesk">
-              <CardImg alt="logo linkedin" src="/linkedin-purple.svg" />
+            <Link href="https://www.linkedin.com/in/joaotelesk" target="_blank">
+              <Button
+                role="button"
+                aria-label="clique me para linkedin"
+                type="button"
+                icon={GrLinkedinOption}
+                size={20}
+                className="btn-secondary"
+                colorNormal="text-purple-500"
+                colorHover="text-white-500"
+              />
             </Link>
-            <Link href="https://github.com/joaotelesk">
-              <CardImg alt="logo github" src="/github-purple.svg" />
+            <Link href="https://github.com/joaotelesk" target="_blank">
+              <Button
+                role="button"
+                aria-label="clique me para github"
+                type="button"
+                icon={AiFillGithub}
+                size={20}
+                className="btn-secondary"
+                colorNormal="text-purple-500"
+                colorHover="text-white-500"
+              />
             </Link>
-            <Link href="https://api.whatsapp.com/send?phone=5585988277110">
-              <CardImg alt="logo whatsapp" src="/whatsapp-purple.svg" />
+            <Link
+              href="https://api.whatsapp.com/send?phone=5585988277110"
+              target="_blank"
+            >
+              <Button
+                role="button"
+                aria-label="clique me para whatsapp"
+                type="button"
+                icon={FaWhatsapp}
+                size={20}
+                className="btn-secondary"
+                colorNormal="text-purple-500"
+                colorHover="text-white-500"
+              />
             </Link>
-            <Link href="https://discord.gg/cDK55jn6">
-              <CardImg alt="logo discord" src="/discord-purple.svg" />
+            <Link href="https://discord.gg/cDK55jn6" target="_blank">
+              <Button
+                role="button"
+                aria-label="clique me para discord"
+                type="button"
+                icon={FaDiscord}
+                size={20}
+                className="btn-secondary"
+                colorNormal="text-purple-500"
+                colorHover="text-white-500"
+              />
             </Link>
           </div>
         </div>
